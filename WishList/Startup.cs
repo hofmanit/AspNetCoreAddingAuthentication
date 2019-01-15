60e8cd6b90c40b7aef95540f0bf3712e8c52b962
Arwin Hofman <a.w.hofman@gmail.com>
2019-01-15 21:05:38 +0100

    Add Authentication Support

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -14,7 +14,7 @@ namespace WishList
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc(); 
+            services.AddMvc();
             services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("Wishlist"));
 
             services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();

7bb198b61173c9d5df8a67ea8a634991aa8e8d00
Arwin Hofman <a.w.hofman@gmail.com>
2019-01-15 22:24:10 +0100

    Create Login Functionality

---
+++ WishList/Models/AccountViewModels/LoginViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace WishList.Models.AccountViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public String Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public String Password { get; set; }
+    }
+}
